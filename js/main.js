7061bd68b7b7a58217336f3060bc2d311ade9bf7
Ramiro <ramiro.eiras@gmail.com>
2024-09-20 20:17:10 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//alert("Hola desde main.js")
+console.error("error");
+
+let X = 10 ;
+const Y = 5 ;
+
+let nombre ="Ramiro" ;
+let edad = 31 ;
+
+X = 15 ;
+
+console.log(X+Y);
+console.log("Tu nombre es: " + nombre + "y tu edad es: " + edad);
+console.log(`Tu nombre es: ${nombre} y tu edad es: ${edad}`);
+
+const NU = prompt(`ingrese su nombre:`);
+
+console.log(NU);
